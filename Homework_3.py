5de7d9fc08f08372644368306481501ef983a1cb
anagmishra <anag.mishra@gmail.com>
2026-08-21 07:27:07 -0700

    Add files via upload

---
+++ Homework_3.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("car_evaluation.csv")
+
+#print(data.head())
+#print(data.info())
+
+x = data[["buying price", "maintenance cost", "lug_boot", "safety"]]
+y = data["decision"]
+
+le = preprocessing.LabelEncoder()
+oe = preprocessing.OrdinalEncoder()
+#data[["buying price", "lug_boot", "safety", "decision"]] = le.fit_transform(data[["buying price", "lug_boot", "safety", "decision"]])
+
+x = oe.fit_transform(x)
+y = le.fit_transform(y)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
+print(y_train.shape)
+
+model = LogisticRegression()
+model.fit(x_train, y_train)
+
+y_predictions = model.predict(x_test)
+
+matrix = confusion_matrix(y_test, y_predictions)
+
+sns.heatmap(matrix, annot = True, fmt = 'd')
+
+plt.title("Confusion Matrix")
+plt.show()
+
+print("Classification Report:")
+print(classification_report(y_test, y_predictions))
